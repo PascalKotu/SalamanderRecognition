9a0e09ba3b77e1cefdef45655462508a735e53dd
Brijeshcr <brijeshguptacr@gmail.com>
2020-04-20 13:01:08 +0530

    Create ImgToVideo.py

---
+++ ImgToVideo.py
@@ -0,0 +1,19 @@
+import cv2
+import numpy as np
+from glob import glob
+
+source = 'Small_Salamander/*.png'
+img_array = []
+#size = ()
+
+for file in glob(source):
+    img = cv2.imread(file)
+    height, width, layers = img.shape
+    size = (width,height)
+    img_array.append(img)
+
+out = cv2.VideoWriter('Small_Salamander/Video/Salamander_Video.avi',cv2.VideoWriter_fourcc(*'DIVX'), 15, size)
+
+for i in range(len(img_array)):
+    out.write(img_array[i])
+out.release()
